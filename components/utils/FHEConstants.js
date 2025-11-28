e69aba95108f1d15ef01e4060ebd592808c2a19a
KalabMesfin <etcipher@gmail.com>
2025-11-28 15:08:45 +0300

    Add FHEConstants.js with contract and config constants

---
+++ components/utils/FHEConstants.js
@@ -0,0 +1,20 @@
+// context/constant.js
+export const CONTRACTS = {
+    FHE_ZKRYPT: '0x02EE6633802FFD5D698BfC38255A109d87fb0e95',
+    SEPOLIA_RPC: 'https://rpc.sepolia.org',
+    FHE_GATEWAY: 'https://gateway.testnet.fhevm.network',
+    FAUCET_DRIP: 1000,
+    TOTAL_SUPPLY: 10000000
+};
+
+export const FHE_CONFIG = {
+    TFHE_VERSION: '0.6.2',
+    ENCRYPTION_TYPE: 'euint32',
+    NETWORK_ID: 11155111
+};
+
+export const UI_CONSTANTS = {
+    LOADING_TIME: 1200,
+    TRUNCATE_CHARS: 6,
+    ZAMA_PREFIX: 'zama_'
+};
